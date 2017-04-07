65238d46757a697d6b3314a702eea556c250cbce
Navneet Nandi <nandinavneet@gmail.com>
2017-04-07 17:45:42 -0400

    Second Commit

--- httpget.py
+++ httpget.py
@@ -1,15 +1,30 @@
 import requests
 import http
+import wave
+import struct
 
-#Requesting 128 random numbers ranging from 1 to 56
-r1 = requests.get("https://www.random.org/integers/?num=128&min=1&max=56&col=1&base=10&format=plain&rnd=new")
+#Requesting random numbers ranging from 1 to 200
+l = []
+for i in range(1,1000):
+    r1 = requests.get("https://www.random.org/integers/?num=8000&min=1&max=200&col=1&base=10&format=plain&rnd=new")
+    a = r1.content
+    b = a.decode("utf-8")
 
-a = r1.content
+    s = b.split()
+    s = list(map(int,s))
+    l = l + s
+print(l)
 
-#print(r1.content)
 
-b = a.decode("utf-8")
+f = wave.open('wavfile.wav', 'w')
+f.setnchannels(1)
+f.setsampwidth(2)
+f.setframerate(6000)
+#f.setparams(2,1,2000,0, 'NONE', 'nothing')
 
-s = b.split()
-s = list(map(int, s))
-print(s)
+for rand_num in l:
+    f.writeframes(struct.pack('h', rand_num))
+
+
+f.close()
+
